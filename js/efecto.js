0766afe315e74afc790f36ceb76bfb84db269e6e
AlbaEspejo <albaespejo90@hotmail.es>
2023-06-13 19:54:31 +0200

    first commit

---
+++ js/efecto.js
@@ -0,0 +1,25 @@
+
+$(document).ready(()=>{
+    $('.facebook').on('mouseover',function () { 
+        $('#facebook').addClass('fa-beat')
+    });
+
+    $('.facebook').on('mouseout',function () { 
+        $('#facebook').removeClass('fa-beat')
+    });
+    $('.twitter').on('mouseover',function () { 
+        $('#twitter').addClass('fa-beat')
+    });
+
+    $('.twitter').on('mouseout',function () { 
+        $('#twitter').removeClass('fa-beat')
+    });
+    $('.youtube').on('mouseover',function () { 
+        $('#youtube').addClass('fa-beat')
+    });
+
+    $('.youtube').on('mouseout',function () { 
+        $('#youtube').removeClass('fa-beat')
+    });
+   })
+
